22e7e0345fc47d08fbb1c034d277c869b421bdcf
alamin-eng <alaminruben@gmail.com>
2024-07-20 20:53:45 +0100

    hero

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Hero from './header/hero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          TV shows (React version)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Start building the app
-        </a>
-      </header>
+     <Hero/>
     </div>
   );
 }
---
+++ src/header/hero.js
@@ -0,0 +1,19 @@
+import './hero.css';
+
+export default function Hero(){
+  return(
+    <div className='hero'>
+    <p>
+          TV shows (React version)
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Start building the app
+        </a>
+        </div>
+  )
+}
