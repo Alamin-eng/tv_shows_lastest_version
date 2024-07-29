1f69c831ce4b50fafbb6c5627559605c1d6641a6
alamin-eng <alaminruben@gmail.com>
2024-07-29 23:10:53 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Hero from "./hero/hero";
 import Header from "./header/header";
 import Main from "./main/main";
+import Footer from "./footer/footer";
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
       <Main />
+      <Footer />
     </div>
   );
 }
