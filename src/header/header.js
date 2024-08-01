d930c4a6c693b8c9464ee6b528471989484c81e0
alamin-eng <alaminruben@gmail.com>
2024-08-01 04:49:36 +0100

    logo added

--- src/header/header.js
+++ src/header/header.js
@@ -1,5 +1,11 @@
 import "./header.css";
 
 export default function Header() {
-  return <div className="header">Logo and menu goes here</div>;
+  return <div className="header">
+  <div>  Navbar here</div>
+  <div className="tv-logo"> </div>
+  
+
+  
+  Logo and menu goes here</div>;
 }
