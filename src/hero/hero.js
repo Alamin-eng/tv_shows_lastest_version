17a5a37ff6fc51f27454442a8feb0c769813a752
alamin-eng <alaminruben@gmail.com>
2024-08-08 20:25:02 +0100

    hero-reedited

--- src/hero/hero.js
+++ src/hero/hero.js
@@ -1,21 +1,19 @@
-import './hero.css';
+import "./hero.css";
 
-export default function Hero(){
-  return(
-    <div className='hero'>
-    <div className="tv-logo"> </div>
-    <h1>H1 HERE</h1>
-    <p>
-          TV shows (React version)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Start building the app
-        </a>
-        </div>
-  )
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div>
+        <h1>SHOWS</h1>
+      </div>
+      <div> Enjoy your favourite classic TV shows full informtion here </div>
+      <div className="hero-description">
+        TV shows guide and information about your favourite classic shows.
+        Thanks to TV MUSE for the generous support of your public API.
+      </div>
+      <div> Get started Button here</div>
+      <div>Have a account? Log in </div>
+      <div>Google play and App store buttons</div>
+    </div>
+  );
 }
