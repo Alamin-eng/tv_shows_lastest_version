a85d329559053d1951751ba35342c2605503a8eb
alamin-eng <alaminruben@gmail.com>
2024-08-31 03:04:07 +0100

    card style completed

--- src/main/main.js
+++ src/main/main.js
@@ -1,29 +1,41 @@
 import "./main.css";
 import { useEffect, useState } from "react";
-
+import { FaStar } from "react-icons/fa";
 const data = require("../shows.json"); // code to fetch api data to replace these static json data
 
 export default function Main() {
   return (
     <div className="main">
-    <h1>{data.length}</h1>
-    
-      {" "}
+      <h1>{data.length}</h1>{" "}
       {data.map((show) => {
         return (
           <div className="main-card">
-            <img className="card-image" src={`${!show.image.original ? show.image.medium : show.image.original}`} alt={`${show.name} original banner`}/>
-            <div className="main-texts"> 
-            <div className="card-name">{show.name && show.name}</div>
-            <div className="card-rating">{show.rating && show.rating.average}</div>
-            <div className="card-country">
-              {!show.network ? "data unavailable" : show.network.country.name}
-            </div>
-            <div style={{ display: "flex", gap: "10px" }}>
-              <span>{show.genres[0]} </span>
-              <span>{show.genres[1]} </span>
-            </div>
-            <div className="main-network">{show.network && show.network.name} </div>
+            <img
+              className="card-image"
+              src={`${
+                !show.image.original ? show.image.medium : show.image.original
+              }`}
+              alt={`${show.name} original banner`}
+            />
+            <div className="main-texts">
+             <div className="name-and-rating-container">
+              <div className="card-name">{show.name && show.name}</div>
+              <div className="card-rating">
+                <FaStar color="yellow" fillOpacity={0.9}/>{" "}
+                <div>{show.rating && show.rating.average}</div>
+              </div>
+              </div>
+              <div className="card-country">
+                {!show.network ? "data unavailable" : show.network.country.name}
+              </div>
+              <div className="card-genre-container">
+                <span className="card-genres">{show.genres[0]} </span>
+                <span className="card-genres">{show.genres[1]} </span>
+                <span className="card-genres">{show.genres[2]} </span>
+              </div>
+              <div className="main-network">
+                {show.network && show.network.name}{" "}
+              </div>
             </div>
           </div>
         );
--- todo.md
+++ todo.md
@@ -1,8 +1,9 @@
 // To do -
 // re-style backward timer
 // add left to right opacity of background so that the image on the right are brighter
-// fix the background images on mobile device, images showing too narrow. done
+// fix the background images on mobile device, images showing too narrow. Done
 // set star and rating on top right of cards
 // resize the card image with better height
 // style the card-texts nicely with spans then set the links for everything. add external link for each cards and remove country.
 // complete the project by 5th august
+// Add modal for each shows summary and useful info
