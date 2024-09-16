03cf61d0b3cd15191403bda0699d69230e6f3192
alamin-eng <alaminruben@gmail.com>
2024-09-16 03:28:23 +0100

    genre 2

--- src/genre/genre.js
+++ src/genre/genre.js
@@ -1,7 +1,16 @@
 import "./main.css";
 
-export default function Genre() {
- return (
-  <div>main genre needs to be break down in to separate function in here</div>
- )
+export default function Genre({ props, handleClick }) {
+  return (
+    <div>
+      main genre needs to be break down in to separate function in here.
+      <div className="card-genre-container">
+        {props.genres.map((genre) => (
+          <button className="card-genres" onClick={handleClick}>
+            {genre}{" "}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
