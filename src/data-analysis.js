17ff5d59a4886375a304082e301e35747df13a1c
alamin-eng <alaminruben@gmail.com>
2024-09-24 01:54:07 +0100

    data-analysis type

--- src/data-analysis.js
+++ src/data-analysis.js
@@ -9,4 +9,16 @@ const filterLanguageUsingIndexof = language.filter((value, index, array) => arra
 const filterUsingSet = [...new Set(language)] // prints array 
 const filterUsingSetReturnsObjL = new Set([...language]) // prints object literal
 console.log(filterLanguageUsingIndexof, filterUsingSetReturnsObjL)
+/* Set(3) { 'English', 'Japanese', 'french' } */
 
+// --- how many types of type --- 
+const type = data.map((el) => el.type)
+const filterType = new Set([...type]) // prints object literal
+console.log(filterType) 
+/* Set(5) {
+  'Scripted',
+  'Reality',
+  'Animation',
+  'Talk Show',
+  'Documentary'
+} */
