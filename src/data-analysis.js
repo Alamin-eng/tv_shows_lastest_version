c8a0419c2e5a34f55033501aea23261aeb922e60
alamin-eng <alaminruben@gmail.com>
2024-09-25 15:07:03 +0100

    data-analysis date

--- src/data-analysis.js
+++ src/data-analysis.js
@@ -2,7 +2,6 @@ const data = require("./shows.json")
 
 // --- how many types of language --- 
 const language = data.map((el) => el.language)
-language.push("french")
 // remove duplicates language  using filter and indexOf
 const filterLanguageUsingIndexof = language.filter((value, index, array) => array.indexOf(value) === index)
 // remove duplicates language using Set method
@@ -22,3 +21,19 @@ console.log(filterType)
   'Talk Show',
   'Documentary'
 } */
+
+// --- how many types of weight --- 
+const weight = data.filter((el) => el.weight === 100).map((el) => el.id)
+
+console.log("id's of the all weight 100 are: " + weight) 
+/* 
+id's of the all weight 100 are: 5,56,59,60,67,81,83,103
+*/
+
+// --- reverse the date of first aired and replace month for name of month --- 
+const premired = data.map((el) => el.premiered)
+const dateMonthYear = premired.map((el) => el.split("-").reverse())
+const replaceNameOfMonth = dateMonthYear.map((el) => {
+  return el[1].replace(/01/g,"January").replace(/02/g,"February").replace(/03/g,"March").replace(/04/g,"April").replace(/05/g,"May").replace(/06/g,"June").replace(/07/g,"July").replace(/08/g,"August").replace(/09/g,"September").replace(/10/g,"October").replace(/11/g,"November").replace(/12/g,"December")
+})
+console.log(replaceNameOfMonth) // date revesed and replaced name of the months
