bbc748fba598e5bd072e2f5adfaa5c6e2649cd54
alamin-eng <alaminruben@gmail.com>
2024-10-02 03:14:03 +0100

    country 1st attempt

--- src/main/country/country.js
+++ src/main/country/country.js
@@ -1,7 +1,7 @@
 import "./county.css";
 
-export default function Country({}){
+export default function Country({country, setCountry, originalData}){
   return(
-    <div> </div>
+    <div>mains country goes here </div>
   )
 };
