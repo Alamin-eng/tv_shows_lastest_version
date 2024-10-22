f0fa9243c03ab1621629ff8bf4f9cff512728ddb
alamin-eng <alaminruben@gmail.com>
2024-10-23 00:47:36 +0100

    show modal structure

--- src/shows modal/modal.js
+++ src/shows modal/modal.js
@@ -1,5 +1,15 @@
 export default function Shows_modal(){
   return(
-    <div>Modal goes here</div>
+    <div>
+      <div> header (Title & run time div's)</div>
+      <div>modal body
+        <div>description</div>
+        <div>Genre</div>
+        <div>country</div>
+      </div>
+      <div>Modal footer
+        <div>official site link</div>
+      </div>
+    </div>
   )
 }
